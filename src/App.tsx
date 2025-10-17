54d0725fe4b011857a20fe45487adfc148dd2f6f
murki05 <baloghmark05@gmail.com>
2025-10-17 11:59:43 +0200

    Készen van a dolgozat Tanár Úr! További szép napot kíván a Magyar Provident Csapata

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import FAQ from "./components/FAQ.tsx";
+import desktop_background from '../assets/images/background-pattern-desktop.svg';
+import mobile_background from '../assets/images/background-pattern-mobile.svg';
+
+
 const App = () => {
   return (
     <main>
+        <img style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        zIndex: -1,
+    }} src={window.innerWidth > 600 ? desktop_background : mobile_background} alt="background"/>
+        <FAQ/>
     </main>
   )
 }
